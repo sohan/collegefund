52c54b9c779d32d6b6b1836db3c35277adcd5d13
Sohan Jain <sj346@cornell.edu>
2012-02-18 17:24:08 -0500

    basic model. requirements now use django nonrel and mongodb

--- collegefund/fund/models.py
+++ collegefund/fund/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from djangotoolbox.fields import ListField
+from djangotoolbox.fields import EmbeddedModelField
+from django.utils.encoding import smart_str
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    university = models.CharField(max_length=500)
+    #photo = models.ImageField(upload_to='photos')
+
+    def __str__(self):
+        return smart_str('%s' % self.name)
+    
+class Achievement(models.Model):
+    student = models.ForeignKey('Student')
+    summary = models.TextField()
+
+    def __str__(self):
+        return smart_str('%s: %s' % (self.student.name, str(self.summary)[:20]))
